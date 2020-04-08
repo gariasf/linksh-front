e47302a163521f5551d6c36d90812856b58a315c
Guillem Arias <accounts@gariasf.com>
2020-04-08 23:44:30 +0200

    Add theming & general style fixes

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,13 +4,50 @@ import { DocumentContent } from "./document-content";
 import { DocumentFooter } from "./document-footer";
 
 import "./styles.scss";
-import { CssBaseline } from "@material-ui/core";
+import {
+  CssBaseline,
+  ThemeProvider,
+  createMuiTheme,
+  useMediaQuery,
+} from "@material-ui/core";
 
-export const Layout = (props: { children: React.ReactNode }) => (
-  <div className="layout">
-    <CssBaseline />
-    <DocumentHead />
-    <DocumentContent>{props.children}</DocumentContent>
-    <DocumentFooter />
-  </div>
-);
+export const Layout = (props: { children: React.ReactNode }) => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        typography: {
+          fontFamily: [
+            '"Nunito Sans"',
+            "-apple-system",
+            "BlinkMacSystemFont",
+            '"Segoe UI"',
+            "Roboto",
+            '"Helvetica Neue"',
+            "Arial",
+            "sans-serif",
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(","),
+        },
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: { main: "#E91E63" },
+          secondary: { main: "#00E5FF" },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <div className="layout">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <DocumentHead />
+        <DocumentContent>{props.children}</DocumentContent>
+        <DocumentFooter />
+      </ThemeProvider>
+    </div>
+  );
+};
