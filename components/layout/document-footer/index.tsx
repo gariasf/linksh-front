5fc9df3df2ce0efc2de812d9f834d67907f8cb09
Guillem Arias <accounts@gariasf.com>
2020-04-11 15:32:11 +0200

    Refactor footer

--- components/layout/document-footer/index.tsx
+++ components/layout/document-footer/index.tsx
@@ -1,72 +1,18 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import Dialog, { DialogProps } from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import useTheme from "@material-ui/core/styles/useTheme";
-import ReactMarkdown from "react-markdown";
-
-import "./styles.scss";
 import { APP_VERSION } from "@core/version";
-
-const CHANGELOG = require("/CHANGELOG.md").default;
+import { Link } from "@material-ui/core";
+import React from "react";
+import "./styles.scss";
 
 export const DocumentFooter = () => {
-  const [open, setOpen] = React.useState(false);
-  const [scroll, setScroll] = React.useState<DialogProps["scroll"]>("paper");
-
-  const handleClickOpen = (scrollType: DialogProps["scroll"]) => () => {
-    setOpen(true);
-    setScroll(scrollType);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const descriptionElementRef = React.useRef<HTMLElement>(null);
-
-  React.useEffect(() => {
-    if (open) {
-      const { current: descriptionElement } = descriptionElementRef;
-      if (descriptionElement !== null) {
-        descriptionElement.focus();
-      }
-    }
-  }, [open]);
-
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
   return (
     <footer>
-      <span>Nethruster</span>&nbsp;-&nbsp;
-      <Button size="small" onClick={handleClickOpen("paper")}>
+      <Link href="https://nethruster.com" target="_blank">
+        <span>Nethruster</span>
+      </Link>
+      &nbsp;-&nbsp;
+      <Link href="https://github.com/nethruster/linksh-front" target="_blank">
         <span>Linksh v{APP_VERSION}</span>
-      </Button>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        scroll={scroll}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">Changelog</DialogTitle>
-        <DialogContent dividers={scroll === "paper"}>
-          <DialogContentText id="scroll-dialog-description" tabIndex={-1}>
-            <ReactMarkdown className="markdown" source={CHANGELOG} />
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+      </Link>
     </footer>
   );
 };
