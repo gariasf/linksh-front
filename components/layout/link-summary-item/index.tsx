e3df03bc1d2eabc26c14806c2dfcedd6de9ec923
Guillem Arias <accounts@gariasf.com>
2020-04-15 18:53:26 +0200

    Link list test

--- components/layout/link-summary-item/index.tsx
+++ components/layout/link-summary-item/index.tsx
@@ -9,6 +9,10 @@ import {
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import FileCopyOutlined from "@material-ui/icons/FileCopyOutlined";
+import CropFreeOutlined from "@material-ui/icons/CropFreeOutlined";
+import VisibilityOutlinedIcon from "@material-ui/icons/VisibilityOutlined";
+import ScheduleOutlinedIcon from "@material-ui/icons/ScheduleOutlined";
+import FingerprintOutlinedIcon from "@material-ui/icons/FingerprintOutlined";
 import "./styles.scss";
 
 export const LinkSummaryItem = () => {
@@ -19,7 +23,11 @@ export const LinkSummaryItem = () => {
     console.log("copy");
   };
   return (
-    <ExpansionPanel elevation={0} TransitionProps={{ unmountOnExit: true }}>
+    <ExpansionPanel
+      elevation={0}
+      TransitionProps={{ unmountOnExit: true }}
+      className="link-summary"
+    >
       <ExpansionPanelSummary
         classes={{ content: "link-summary-header" }}
         expandIcon={<ExpandMoreIcon />}
@@ -36,42 +44,71 @@ export const LinkSummaryItem = () => {
         </Link>
         <div className="subheading">
           <Typography variant="body2" noWrap>
-            https://www.byte-time.com/viewtopic.php?f=5&t=5
+            <Link
+              color="secondary"
+              href="https://www.byte-time.com/viewtopic.php?f=5&t=5"
+            >
+              https://www.byte-time.com/viewtopic.php?f=5&t=5
+            </Link>
           </Typography>
         </div>
+        <IconButton>
+          <CropFreeOutlined fontSize="small" />
+        </IconButton>
         <IconButton onClick={handleCopyClick}>
           <FileCopyOutlined fontSize="small" />
         </IconButton>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <div style={{ display: "flex", flex: 1 }}>
+        <div
+          style={{ display: "flex", flex: 1, justifyContent: "space-evenly" }}
+        >
           <div className="column">
-            <Typography variant="body2">
-              Original:&nbsp;
-              <Link
-                color="secondary"
-                href="https://www.byte-time.com/viewtopic.php?f=5&t=5"
-              >
-                https://www.byte-time.com/viewtopic.php?f=5&t=5
-              </Link>
+            <div className="column-title">
+              <FingerprintOutlinedIcon
+                fontSize="small"
+                style={{ color: "rgba(0, 0, 0, 0.23)" }}
+              />
+              &nbsp;
+              <Typography variant="button" align="center">
+                ID
+              </Typography>
+            </div>
+            <Typography variant="overline" align="center">
+              123456
             </Typography>
-            <Typography variant="body2">Visits:&nbsp;53</Typography>
           </div>
           <div className="column">
-            <Typography variant="body2">Created on:&nbsp;Time</Typography>
-            <Typography variant="body2">
-              Last time visited:&nbsp;Time
+            <div className="column-title">
+              <VisibilityOutlinedIcon
+                fontSize="small"
+                style={{ color: "rgba(0, 0, 0, 0.23)" }}
+              />
+              &nbsp;
+              <Typography variant="button" align="center">
+                Visits
+              </Typography>
+            </div>
+            <Typography variant="overline" align="center">
+              53
             </Typography>
           </div>
-          <div className="helper column">
-            <Typography variant="caption">
-              Select your destination of choice
-              <br />
-              <a href="#secondary-heading-and-columns">Learn more</a>
+          <div className="column">
+            <div className="column-title">
+              <ScheduleOutlinedIcon
+                fontSize="small"
+                style={{ color: "rgba(0, 0, 0, 0.23)" }}
+              />
+              &nbsp;
+              <Typography variant="button" align="center">
+                Created
+              </Typography>
+            </div>
+            <Typography variant="overline" align="center">
+              Yesterday
             </Typography>
           </div>
         </div>
-        <Divider />
       </ExpansionPanelDetails>
     </ExpansionPanel>
   );
